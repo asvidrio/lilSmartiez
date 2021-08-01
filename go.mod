3035dae7bd5a9c1e469a7cc6bd33cecdc3cbeb3e
burrell <eewinterz@gmail.com>
2021-08-01 02:04:29 -0400

    constructed the database and define models for resources and user accounts

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/asvidrio/lilSmartiez
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.7.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+)
--- server.go
+++ server.go
@@ -1,28 +1,85 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
+	"fmt"
+	"context"
+	"time"
+	"os"
+	// "github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var PORT = ":9000"
 
+var Users []struct {
+	Id primitive.ObjectID
+	Email string	
+	Username string			
+	Password string			
+}
+
+var Resource []struct {
+	Id int
+	Author string 
+	Title string 
+	Description string 
+	Type string 
+	Date string 
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
 func main() {
-	r := gin.Default()
-	r.Run(PORT)
+	mongodb := goDotEnvVariable("MONGO_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongodb))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	users, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(users, resources)
 }
 
-func addResource(){
+// func addResource(c *gin.Context){
 	
-}
+// }
 
-func removeResource() {
+// func findResource(c *gin.Context) {
 
-}
+// }
 
-func updateResource() {
+// func updateResource(c *gin.Context) {
 
-}
+// }
 
-func deleteResource() {
+// func removeResource(c *gin.Context) {
 
-}
+// }
