2c0497ce5483f4976e97b2e0ab32d099b71431e8
burrell <eewinterz@gmail.com>
2021-08-01 21:38:39 -0400

    testing

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 	"errors"
+	"github.com/asvidrio/lilSmartiez/routes"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -47,11 +48,12 @@ func goDotEnvVariable(key string) string {
 func main() {
 	// routing logic
 	router := gin.Default()
-	router.GET("/api/resource/:id", 	findResource)
+	router.GET("/api/resource/:id", 	routes.findResource)
 	router.GET("/api/resource/", 		findAllResources)
 	router.POST("/api/resource/", 		addResource)
 	router.PUT("/api/resource/", 		updateResource)
 	router.DELETE("/api/resource/:id", 	removeResource)
+
 
 	// router.GET("/api/users/:id", 		findUser)
 	// router.GET("/api/users/", 			findAllUsers)
@@ -87,109 +89,6 @@ func connectToDatabase()(*mongo.Client, context.Context, context.CancelFunc) {
 	}
 	fmt.Println(users, resources)
 	return client, ctx, cancel
-}
-
-func addResource(c *gin.Context){
-	var resource Resource
-	if err := c.ShouldBindJSON(&resource); err != nil {
-		fmt.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"[ERROR] ":err})
-		return
-	}
-	id, err := addDBResource(&resource)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"[ERROR]": err})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"[Successfully created a new resource]":id})
-}
-
-func addDBResource(resource *Resource)(primitive.ObjectID, error){
-	client, ctx, cancel := connectToDatabase()
-	defer cancel()
-	defer client.Disconnect(ctx)
-	resource.Id = primitive.NewObjectID()
-	data, err := client.Database("smartiez").Collection("resources").InsertOne(ctx, resource)
-	if err != nil {
-		fmt.Printf("[ERROR] Could not create the resource: %v", err)
-		return primitive.NilObjectID, err
-	}
-	id := data.InsertedID.(primitive.ObjectID)
-	return id, nil
-}
-
-func findResource(c *gin.Context)() {
-	var resource Resource
-	if err := c.BindUri(&resource); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"[ERROR2]":err})
-		return
-	}
-	var data, err = findDBResource(resource.Id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"[ERROR3]": err})
-		return 
-	}
-	c.JSON(http.StatusOK, 
-		gin.H{
-			"Id":data.Id, 
-			"Author":data.Author, 
-			"Title":data.Title, 
-			"Description":data.Description, 
-			"Type":data.Type,
-			"Date":data.Date,
-		})
-}
-
-func findDBResource(id primitive.ObjectID)(*Resource, error){
-	var resource *Resource
-	client, ctx, cancel := connectToDatabase()
-	defer cancel()
-	defer client.Disconnect(ctx)
-	data := client.Database("smartiez").Collection("resources").FindOne(ctx, bson.D{})
-	if data == nil {
-		return nil, errors.New("[ERROR FOUND1]")
-	}
-	err := data.Decode(&resource)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	fmt.Printf("Resource: %v", resource)
-	return resource, nil
-}
-
-func findAllResources(c *gin.Context)(){
-	var resources, err = findAllDBResources()
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"[ERROR FOUND5]":err})
-	}
-	c.JSON(http.StatusOK, gin.H{"available resources":resources})
-}
-
-func findAllDBResources()([]*Resource, error) {
-	var resources []*Resource
-	client, ctx, cancel := connectToDatabase()
-	defer cancel()
-	defer client.Disconnect(ctx)
-	data, err := client.Database("smartiez").Collection("resources").Find(ctx, bson.D{})
-	if err != nil {
-		return nil, err
-	}
-	defer data.Close(ctx)
-	err = data.All(ctx, &resources)
-	if err != nil {
-		fmt.Println("[ERROR 8]", err)
-		return nil, err
-	}
-	return resources, nil
-}
-
-func updateResource(c *gin.Context) {
-
-}
-
-func removeResource(c *gin.Context) {
-
 }
 
 // 
